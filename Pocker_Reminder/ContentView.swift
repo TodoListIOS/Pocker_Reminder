e508e2c617db92379acc6cad4b1ffcc4a2364fcf
cai rui <rui.cai2020@outlook.com>
2019-12-27 21:34:00 +0800

    Initial Commit

---
+++ Pocker_Reminder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pocker_Reminder
+//
+//  Created by rui cai on 2019/12/27.
+//  Copyright © 2019 Buct. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
