e363a28ecbc3c467a559131f23fa875702acd3ba
cai rui <rui.cai2020@outlook.com>
2020-01-03 10:44:18 +0800

    add net package

--- Pocker_Reminder/ContentView.swift
+++ Pocker_Reminder/ContentView.swift
@@ -7,10 +7,28 @@
 //
 
 import SwiftUI
+import Alamofire
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            Button(action: {
+                Alamofire.request("https://httpbin.org/get")
+                        .responseJSON { response in
+                            print(response.request)  // original URL request
+                            print(response.response) // URL response
+                            print(response.data)     // server data
+                            print(response.result)   // result of response serialization
+                
+                            if let JSON = response.result.value {
+                                print("JSON: \(JSON)") //具体如何解析json内容可看下方“响应处理”部分
+                            }
+                }
+            }) {
+                Text(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/)
+            }
+        }
+        //Text("Hello, World!")
     }
 }
 
